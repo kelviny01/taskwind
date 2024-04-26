49881da81ba95bd10d03c303544ab78f4f266cef
kelviny01 <kelviny.mk27@gmail.com>
2024-04-26 09:27:09 -0300

    mobile responsiveness completed

--- src/pages/app/landing-page/landing-page.tsx
+++ src/pages/app/landing-page/landing-page.tsx
@@ -5,7 +5,7 @@ import undrawIlustration from "../../../assets/undrawIlustration.svg";
 
 export function LandingPage() {
   return (
-    <div className="w-full h-screen px-20 pt-8">
+    <div className="sm:px-6 pt-8 w-full h-screen">
       <header className="flex justify-between items-center">
         <div className="flex items-center gap-x-4">
           <img src={logo} alt="" />
@@ -17,26 +17,26 @@ export function LandingPage() {
           </Link>
         </div>
       </header>
-      <section className="flex items-center justify-between h-full">
+      <section className="sm:flex-col items-center justify-between h-full sm:my-24">
         <div className="flex flex-col max-w-2xl">
-          <h1 className="text-7xl font-bold leading-tight">
+          <h1 className="sm:text-4xl sm:leading-snug font-bold ">
             Gerenciamento de Tarefas{" "}
-            <span className="text-white bg-gradient-to-l from-purple-600 to-purple-400 rounded-3xl px-6">
+            <span className="sm:rounded-xl text-white bg-gradient-to-l from-purple-600 to-purple-400 px-6">
               Eficaz.
             </span>
           </h1>
-          <p className="text-lg font-bold mt-5 mb-7 max-w-md">
+          <p className="sm:pt-4 sm:font-normal text-lg max-w-md">
             Aumente a eficiência do seu trabalho e reduza o tempo necessário
             para gerenciar tarefas com nossos sistema.
           </p>
           <Link
             to="/login"
-            className="text-white font-bold bg-gradient-to-r from-purple-600 to-purple-400 rounded-lg px-6 max-w-fit py-4 px-4"
+            className="text-white font-bold bg-gradient-to-r from-purple-600 to-purple-400 rounded-lg px-6 max-w-fit py-4 px-4 sm:mt-3"
           >
             Começar Agora
           </Link>
         </div>
-        <div>
+        <div className="sm:mt-16">
           <img src={undrawIlustration} alt="" />
         </div>
       </section>
